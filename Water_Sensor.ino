655f123f35fdbdf3292eb3bdbfa90c21d72345ab
KheeYaw <79307366+KY-Aegis@users.noreply.github.com>
2021-12-09 23:16:35 +0800

    Added Comments

--- Water_Sensor.ino
+++ Water_Sensor.ino
@@ -1,58 +1,59 @@
-#include <Wire.h>
-#include <LCD_I2C.h>
-LCD_I2C lcd(0x27, 16, 2);
-int sensorVal=0;//variable to store sensor value
-int sensorPin=A4;//variable for sensor pin
-int lowerLimit=600;
-int upperLimit=650;
-int red=0;
-int yellow=1;
-int green=2;
+#include <Wire.h>           //import wire library
+#include <LCD_I2C.h>        //import lcd library
+LCD_I2C lcd(0x27, 16, 2);   //initialize the lcd
+int sensorVal=0;            //variable to store sensor value
+int sensorPin=A4;           //variable for sensor pin
+int lowerLimit=600;         //declare the lowerLimit
+int upperLimit=650;         //declare the upperLimit
+int red=0;                  //declare the pin for the red LED
+int yellow=1;               //declare the pin for the yellow LED
+int green=2;                //declare the pin for the green LED
 
-void setup(){
-  Serial.begin(9600);
-  lcd.begin();
-  lcd.backlight();
-  for (int i=8;i<11;i++){
-    pinMode(i,OUTPUT); 
+void setup(){               //function to initialize I/O
+  Serial.begin(9600);       //initialize the serial import
+  lcd.begin();              //start the led
+  lcd.backlight();          //on the backlight of the led
+  for (int i=8;i<11;i++){   //loop 3 times to activate the pins
+    pinMode(i,OUTPUT);      //initialize the pin i as output
   }
   
 }
 
-void lightUp(int light){
-  for (int i=8;i<11;i++){
-    if (i!=light){
-      digitalWrite(i,LOW);
+void lightUp(int light){        //function to control the pattern of the LEDs
+  for (int i=8;i<11;i++){       //loop 3 times
+    if (i!=light){              //if i is not equals to the pin passed from the main function
+      digitalWrite(i,LOW);      //turn off the other LED
     }
-    else{
-      digitalWrite(i,HIGH);
+    else{                       //if i is equals to the pin passed from the main function
+      digitalWrite(i,HIGH);     //turn on the LED
     }
   }
 }
-void loop(){
-  sensorVal=analogRead(sensorPin);//read the analog on pin A5
-  lcd.clear();
-  lcd.print("Water level:");
-  lcd.setCursor(0,1); 
-  Serial.println(sensorVal);
-  if (sensorVal>-1 && sensorVal<=20){ 
-    lcd.print("Empty");
-    lightUp(8);
-    delay(500);
+
+void loop(){                            //main function
+  sensorVal=analogRead(sensorPin);      //read the analog pin
+  lcd.clear();                          //clear the output of the LCD
+  lcd.print("Water level:");            //display water level on the LCD
+  lcd.setCursor(0,1);                   //start a new line on the LCD
+  Serial.println(sensorVal);            //print the water level at the serial monitor
+  if (sensorVal>-1 && sensorVal<=20){   //if the value is between 0 and 20
+    lcd.print("Empty");                 //display Empty on the LCD
+    lightUp(8);                         //light up the red LED
+    delay(500);                         //delay for 500 milliseconds
   }
-  else if (sensorVal>20 && sensorVal<=lowerLimit){ 
-    lcd.print("Low");
-    lightUp(8);
-    delay(500);
+  else if (sensorVal>20 && sensorVal<=lowerLimit){//if the value is between 21 and lowerLimit
+    lcd.print("Low");   //display Low on the LCD
+    lightUp(8);         //light up the red LED
+    delay(500);         //delay for 500 milliseconds
   }
-  else if (sensorVal>lowerLimit && sensorVal<=upperLimit){ 
-    lcd.print("Normal");
-    lightUp(9);
-    delay(500);
+  else if (sensorVal>lowerLimit && sensorVal<=upperLimit){//if the value is between lowerLimit and upperlimit
+    lcd.print("Normal");    //display Normal on the LCD
+    lightUp(9);             //light up the yellow LED
+    delay(500);             //delay for 500 milliseconds
   }
-  else if (sensorVal>upperLimit){  
-    lcd.print("Full");
-    lightUp(10);
-    delay(500);
+  else if (sensorVal>upperLimit){//if the value is more than the upperlimit
+    lcd.print("Full");  //display Full on the LCD
+    lightUp(10);        //light up the green LED
+    delay(500);         //delay for 500 milliseconds
   } 
 }
